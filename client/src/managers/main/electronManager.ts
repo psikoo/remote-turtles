47c8dfebed9b14975635374fc364bb7ffb275579
psikoo <91342186+psikoo@users.noreply.github.com>
2026-01-27 11:40:35 +0100

    a

--- client/src/managers/main/electronManager.ts
+++ client/src/managers/main/electronManager.ts
@@ -12,27 +12,29 @@ export const electronManager = {
     if (started) { app.quit(); }
 
     app.whenReady().then(() => {
-      createWindow();
+      createWindow(openDevTools);
       mainWindow.webContents.on("did-finish-load", async () => { 
         const worldData = await DatabaseManager.getFullWorld();
         if(worldData) mainWindow.webContents.send("initial-world-load", worldData);
         TurtleManager.disconnectAll();
       });
-      if(openDevTools) mainWindow.webContents.openDevTools();
     });
     
     app.on("window-all-closed", () => { app.quit(); });
-    app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow(); } });
+    app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow(openDevTools); } });
   },
 }
 
-function createWindow() {
+function createWindow(openDevTools: boolean) {
   mainWindow = new BrowserWindow({
     width: 1000, height: 700,
     webPreferences: { preload: path.join(__dirname, "preload.js") },
     icon: "./assets/icon/icon.png"
   });
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+
+  if(openDevTools) mainWindow.webContents.openDevTools({ mode: 'detach' });
+
+  if(MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
